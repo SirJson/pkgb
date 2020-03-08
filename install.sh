202b3e0c775f3c8e33ad6e5224454adfed5a6555
Raffael Zica <raffael@pixeltastic.de>
2020-03-08 19:28:39 +0100

    Restored convinience and renamed script
    Because calling it fzf something would imply that it is an addon.

--- install.sh
+++ install.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
-ORIGIN=$PWD
 SCRIPTPATH="$(
     cd "$(dirname "$0")" >/dev/null 2>&1 || exit
     pwd -P
 )"
-echo "Install for every user"
 if [[ $EUID -ne 0 ]]; then
-     echo "This script must be run as root" 
-     exit 1
+    echo "This script must be run as root"
+    exit 1
 fi
-chmod +x "$SCRIPTPATH/fzf-dpkg"
-ln -s "$SCRIPTPATH/fzf-dpkg" /usr/local/bin/fzf-dpkg
+TARGET="/usr/local/bin/pkgb"
+OLDTARGET="/usr/local/bin/fzf-dpkg"
 
+if [[ -L $OLDTARGET ]]; then
+    echo "Removing old version $OLDTARGET"
+    rm $OLDTARGET
+fi
+
+if [[ -L $TARGET ]]; then
+    echo "Removing old version $TARGET"
+    rm $TARGET
+fi
+
+echo "Install $TARGET"
+chmod +x "$SCRIPTPATH/pkgb"
+ln -s "$SCRIPTPATH/pkgb" "$TARGET"
